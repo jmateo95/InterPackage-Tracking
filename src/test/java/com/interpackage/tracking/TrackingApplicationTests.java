40725f2581e0112e98fc7da7ed621b24cc16b9a9
Fatima Tezo <ftezo@nabenik.dev>
2023-05-06 00:07:57 -0600

    ADD: Code coverage (Jacoco), JunitTest, Mockito and Docker testcontainers

--- src/test/java/com/interpackage/tracking/TrackingApplicationTests.java
+++ src/test/java/com/interpackage/tracking/TrackingApplicationTests.java
@@ -1,10 +1,10 @@
 package com.interpackage.tracking;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
-@SpringBootTest
-class TrackingApplicationTests {
+@Testcontainers
+class TrackingApplicationTests extends AbstractIntegrationTest{
 
     @Test
     void contextLoads() {
