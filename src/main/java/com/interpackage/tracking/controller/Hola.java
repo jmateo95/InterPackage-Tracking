37005ce09d6c57dc7740d49fb0ce160b64b7a6b5
jmateo95 <201313852@ingenieria.usac.edu.gt>
2023-05-21 10:06:48 -0600

    cambios

--- src/main/java/com/interpackage/tracking/controller/Hola.java
+++ src/main/java/com/interpackage/tracking/controller/Hola.java
@@ -1,7 +1,5 @@
 package com.interpackage.tracking.controller;
-//import com.interpackage.tracking.aspect.RequiredPermission;
-
-import com.interpackage.basedomains.aspect.RequiredRole;
+import com.interpackage.tracking.aspect.RequiredRole;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
